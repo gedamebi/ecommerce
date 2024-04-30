f8eed7eb178fd0b62390f21590b361292abfc6f1
German Medina <urugerman@gmail.com>
2024-04-30 14:20:02 -0300

    Actualizo a entrega 2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return(
         <div>
             <Carousel />
-            <h1>Bienvenido a Macrum S.R.L</h1>
+            <h1 className='mensajeBienvenida'>Bienvenido a Macrum S.R.L</h1>
             <p className='primerParrafo'>Gracias por visitar nuestro nuevo sitio web, aqu&iacute; podr&aacute;n enterarse sobre novedades vinculadas a nuestra tarea.</p>
             <p>Sabr&aacute;n sobre nuestros trabajos, los servicios que brindamos y productos por los que trabajamos habitualmente.</p>
             <p>De a poco iremos actualizando nuestro sitio, publicando informaci&oacute;n actualizadas de interes, catalogos y hojas tecnicas sobre productos que trabajamos y consultas frecuentes que recibamos.</p>
